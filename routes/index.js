11d754626551403bc4b37ef038a77dfb1adb89eb
massiv1984 <emil.kero@elev.ga.ntig.se>
2024-02-15 11:16:07 +0100

    bean routes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,23 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', function (req, res) {
-  res.render('index.njk', { title: 'Welcome' })
+  res.render('index.njk', { title: 'Välkommen' })
+})
+
+router.get('/beans', function (req, res) {
+  res.render('beans.njk', { title: 'Bönor' })
+})
+
+router.get('/beans/:id', function (req, res) {
+  res.render('beans.njk', { title: 'Bönor' })
+})
+
+router.get('/reviews', function (req, res) {
+  res.render('beans.njk', { title: 'Recensioner' })
+})
+
+router.get('/reviews/:id', function (req, res) {
+  res.render('beans.njk', { title: 'Recensioner' })
 })
 
 module.exports = router
