bcd41c05cabce0a20e715c8f1bdca45f2ca413ce
massiv1984 <emil.kero@elev.ga.ntig.se>
2024-02-15 10:29:45 +0100

    databas

--- db.js
+++ db.js
@@ -6,6 +6,7 @@ const pool = mysql.createPool({
   queueLimit: 0,
   charset: 'utf8mb4',
   host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
   user: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_DATABASE,
